359bfb7ab69aa185e0a536383e3571961e8babe3
CTAPCKPIM <novickdanya@gmail.com>
2022-09-19 19:11:58 +0300

    Added gas price functions

--- script.py
+++ script.py
@@ -5,76 +5,139 @@ import json
 import os
 import cryptocompare as cc
 
+from web3 import Web3
+from web3.gas_strategies.rpc import rpc_gas_price_strategy
 from pycoingecko import CoinGeckoAPI
 from dotenv import load_dotenv
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 
+""" All variables
 
-# load the configuration from a .env
+'load_dotenv()'' - load the configuration from a .env;
+'headers' - requesting JSON from the site;
+'API' - loading the variable;
+'web3' - connecting Web3;
+'eth_decimals' - decimals of USDC in ETH network;
+"""
 load_dotenv()
 
-# loading the variable
-API = os.getenv('API_KEY')
-
-# requesting JSON from the site
 headers = { 
 	'Accepts': 'application/json'
 }
+# Dotenv variables
+API = os.getenv('API_KEY')
+INFURA = os.getenv('INFURA_URL')
+SECRET = os.getenv('SECRET_KEY')
 
+# Connecting providers
 ccobj = cc.cryptocompare._set_api_key_parameter(API)
 cg = CoinGeckoAPI()
+w3 = Web3(Web3.HTTPProvider(INFURA))
+w3.eth.set_gas_price_strategy(rpc_gas_price_strategy)
 
-''' Ethereum network '''
-def PriceFromCryptocompare():
+eth_decimals = 6
+
+etherscan = 'https://api.etherscan.com/api?module=gastracker&action=gasoracle&apikey=YourApiKeyToken'
+
+#print(w3.isConnected())
+limit = 0
+#print('Limit: ', limit)
+
+def getGasLimit():
 	session = Session()
 	session.headers.update(headers)
-	currency = ['CRV', 'WETH', 'FARM'] #array of crypto currencys(CC)
 	try:
-		for i in currency:
+		response = session.get(etherscan)  # for fantom
+		data_ETH = json.loads(response.text)
+		if (data_ETH['status'] == '1'):
+			return int(float(data_ETH['result']['FastGasPrice']) * 1e9)  
+		else: return 0
+	except (ConnectionError, Timeout, TooManyRedirects, KeyError):
+		return 0
+
+limit = getGasLimit()
+#print('New limit: ', limit)
+#print('Type: ', type(limit))
+
+""" Function parsing price of the gas
+
+'data_ETH' - api.etherscan.com;
+"""
+def getGasPrise():
+	session = Session()
+	session.headers.update(headers)
+	try:
+		response = session.get(etherscan)
+		data_ETH = json.loads(response.text)
+		import pdb; pdb.set_trace()  #pdb.set_trace()
+		gas_price = int(float(data_ETH['result']['FastGasPrice']) * 1e9)
+		print('Gas price: ', gas_price) 
+		print('Type: ', type(gas_price))
+		if (data_ETH['status'] == '1' and gas_price < limit * 5):
+			print('Normal price')
+			return gas_price
+		elif(data_ETH['status'] == '1' and gas_price > limit * 5):  #  for fantom
+			print('Price * 5') 
+			return gas_price * 5
+		else: return 0
+	except (ConnectionError, Timeout, TooManyRedirects, KeyError):
+		return 0
+
+getGasPrise()
+#print(a)
+
+""" Function parsing price of the cryptocurrency in USD from 'Cryptocompare'
+
+'currency_eth' - array of crypto currencys(CC);
+'data' - converting it to a dictionary;
+'price' - turn the USD price into a number with a power of 6;
+"""
+def priceFromCryptocompare():
+	session = Session()
+	session.headers.update(headers)
+	currency_eth = ['CRV', 'WETH', 'FARM']  
+	try:
+		''' Ethereum network (USDC 6 decimals)'''
+		for i in currency_eth:
 			#print(i)
 			response = session.get('https://min-api.cryptocompare.com/data/price?fsym='+ i +'&tsyms=USD')
-			data = json.loads(response.text) #converting it to a dictionary
-			print(i, ': ', data['USD'])
+			data = json.loads(response.text) 
+			#print(i, ': ', data['USD'])
+			price = int(data['USD'] * pow(10, eth_decimals)) 
+			print(price)
 	except (ConnectionError, Timeout, TooManyRedirects, KeyError):
 		print('Error: connecting not successful(Cryptocompare)')
 		return 0
 
-PriceFromCryptocompare()
+#priceFromCryptocompare()
 
-def PriceFromCoinGecko():
+""" Function parsing price of the cryptocurrency in USD from 'CoinGecko'
+
+'currency_eth' - array of crypto currencys(CG);
+'data' - converting it to a dictionary;
+'usd_' - the price from JSON;
+'price' - turn the USD price into a number with a power of 6;
+"""
+def priceFromCoinGecko():
 	session = Session()
 	session.headers.update(headers)
-	currency = ['curve-dao-token', 'weth', 'harvest-finance'] #array of crypto currencys(CG)
+	currency_eth = ['curve-dao-token', 'weth', 'harvest-finance']  
 	try:
-		for i in currency:
+		''' Ethereum network (USDC 6 decimals)'''
+		for i in currency_eth:
 			#print(i)
 			response = session.get('https://api.coingecko.com/api/v3/coins/' + i)
 			data = json.loads(response.text)
-			print(i, ': ', data['market_data']['current_price']['usd'])
+			#print(i, ': ', data['market_data']['current_price']['usd'])
+			usd_ = data['market_data']['current_price']['usd']
+			price = int(usd_ * pow(10, eth_decimals)) 
+			print(price)
 	except (ConnectionError, Timeout, TooManyRedirects, KeyError):
 		print('Error: connecting not successful(CoinGecko)')
 		return 0
 
-PriceFromCoinGecko()
+#priceFromCoinGecko()
 
 ''' Other network '''
 
-
-'''
-#print('\nCryptocompare:')
-#print('|',cc.get_price('CRV', currency='USD'))
-#print('|',cc.get_price('WETH', currency='USD'))
-#print('|',cc.get_price('FARM', currency='USD'))
-
-#print('\nCoinGecko:')
-#print('|',cg.get_price(ids='curve-dao-token', vs_currencies='usd'))
-#print('|',cg.get_price(ids='weth', vs_currencies='usd'))
-#print('|',cg.get_price(ids='harvest-finance', vs_currencies='usd'))
-
-while True:
-	print(cc.get_price('BNB', currency='USD'))
-
-for i in cc.get_coin_list(format=False):
-	print(i)
-'''	
