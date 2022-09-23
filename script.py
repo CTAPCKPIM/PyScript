bad6f4947929470c98d61641dad8f788436a37d8
CTAPCKPIM <novickdanya@gmail.com>
2022-09-23 17:21:40 +0300

    Transaction from the script(test)

--- script.py
+++ script.py
@@ -35,61 +35,76 @@ ccobj = cc.cryptocompare._set_api_key_parameter(API)
 cg = CoinGeckoAPI()
 w3 = Web3(Web3.HTTPProvider(INFURA))
 w3.eth.set_gas_price_strategy(rpc_gas_price_strategy)
+address = '0xf39D262c556aBefFC619847eE0355af42c4d0e91'
 
 eth_decimals = 6
 
 etherscan = 'https://api.etherscan.com/api?module=gastracker&action=gasoracle&apikey=YourApiKeyToken'
 
-#print(w3.isConnected())
-limit = 0
-#print('Limit: ', limit)
+''' ABIs and address of smart contracts(Ethereum network) '''
+abi_crv = json.loads('[{"inputs":[{"internalType":"address","name":"_addrCRV","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"addrCRV","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"priceUSD","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"returnName","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"returnSymbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_priceUSD","type":"uint256"}],"name":"setPriceInUSD","outputs":[],"stateMutability":"nonpayable","type":"function"}]')
+abi_weth = json.loads('[{"inputs":[{"internalType":"address","name":"_addrWETH","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"addrWETH","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"priceUSD","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"returnName","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"returnSymbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_priceUSD","type":"uint256"}],"name":"setPriceInUSD","outputs":[],"stateMutability":"nonpayable","type":"function"}]')
+abi_farm = json.loads('[{"inputs":[{"internalType":"address","name":"_addrFARM","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"addrFARM","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"priceUSD","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"returnName","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"returnSymbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_priceUSD","type":"uint256"}],"name":"setPriceInUSD","outputs":[],"stateMutability":"nonpayable","type":"function"}]')
 
+address_crv = '0x6DE6013B8Ea9fA78DB10e7D7e4D9E66668FaFAae'
+address_weth = '0x71815DcE02aAb149048e68c1EC777331fC890462'
+address_farm = '0xA64e04F3309794A7353894465B3e11D0007115aC'
+
+crv = w3.eth.contract(address=address_crv, abi=abi_crv)
+weth = w3.eth.contract(address=address_weth, abi=abi_weth)
+farm = w3.eth.contract(address=address_farm, abi=abi_farm)
+
+''' Other ABIs and adress '''
+
+#limit = 0
+
+""" Function for a control price of the gas 'Fantom' network """
 def getGasLimit():
 	session = Session()
 	session.headers.update(headers)
 	try:
 		response = session.get(etherscan)  # for fantom
-		data_ETH = json.loads(response.text)
-		if (data_ETH['status'] == '1'):
-			return int(float(data_ETH['result']['FastGasPrice']) * 1e9)  
+		data_eth = json.loads(response.text)
+		if (data_eth['status'] == '1'):
+			return int(float(data_eth['result']['FastGasPrice']) * 1e9)  
 		else: return 0
 	except (ConnectionError, Timeout, TooManyRedirects, KeyError):
 		return 0
 
-limit = getGasLimit()
+#limit = getGasLimit()
 #print('New limit: ', limit)
 #print('Type: ', type(limit))
 
 """ Function parsing price of the gas
 
-'data_ETH' - api.etherscan.com;
+'data_eth' - api.etherscan.com;
 """
 def getGasPrise():
 	session = Session()
 	session.headers.update(headers)
 	try:
 		response = session.get(etherscan)
-		data_ETH = json.loads(response.text)
-		import pdb; pdb.set_trace()  #pdb.set_trace()
-		gas_price = int(float(data_ETH['result']['FastGasPrice']) * 1e9)
-		print('Gas price: ', gas_price) 
-		print('Type: ', type(gas_price))
-		if (data_ETH['status'] == '1' and gas_price < limit * 5):
+		data_eth = json.loads(response.text)
+		#import pdb; pdb.set_trace() 
+		gas_price = int(float(data_eth['result']['FastGasPrice']) * 1e9)
+		#print('Gas price: ', gas_price) 
+		#print('Type: ', type(gas_price))
+		if (data_eth['status'] == '1'): #and gas_price < limit * 5):  #  for Fantom network
 			print('Normal price')
 			return gas_price
-		elif(data_ETH['status'] == '1' and gas_price > limit * 5):  #  for fantom
-			print('Price * 5') 
-			return gas_price * 5
+		#elif(data_eth['status'] == '1' and gas_price > limit * 5):  #  for Fantom network
+			#print('Price * 5') 
+			#return gas_price * 5
 		else: return 0
 	except (ConnectionError, Timeout, TooManyRedirects, KeyError):
 		return 0
 
-getGasPrise()
-#print(a)
+#price = getGasPrise()
+#print('Price of gas: ',price)
 
 """ Function parsing price of the cryptocurrency in USD from 'Cryptocompare'
 
-'currency_eth' - array of crypto currencys(CC);
+'currency_eth' - array of crypto currencys(CC)
 'data' - converting it to a dictionary;
 'price' - turn the USD price into a number with a power of 6;
 """
@@ -100,12 +115,9 @@ def priceFromCryptocompare():
 	try:
 		''' Ethereum network (USDC 6 decimals)'''
 		for i in currency_eth:
-			#print(i)
 			response = session.get('https://min-api.cryptocompare.com/data/price?fsym='+ i +'&tsyms=USD')
-			data = json.loads(response.text) 
-			#print(i, ': ', data['USD'])
-			price = int(data['USD'] * pow(10, eth_decimals)) 
-			print(price)
+			data_eth = json.loads(response.text) 
+			price = int(data_eth['USD'] * pow(10, eth_decimals)) 
 	except (ConnectionError, Timeout, TooManyRedirects, KeyError):
 		print('Error: connecting not successful(Cryptocompare)')
 		return 0
@@ -126,18 +138,71 @@ def priceFromCoinGecko():
 	try:
 		''' Ethereum network (USDC 6 decimals)'''
 		for i in currency_eth:
-			#print(i)
 			response = session.get('https://api.coingecko.com/api/v3/coins/' + i)
-			data = json.loads(response.text)
-			#print(i, ': ', data['market_data']['current_price']['usd'])
-			usd_ = data['market_data']['current_price']['usd']
+			data_eth = json.loads(response.text)
+			usd_ = data_eth['market_data']['current_price']['usd']
 			price = int(usd_ * pow(10, eth_decimals)) 
-			print(price)
 	except (ConnectionError, Timeout, TooManyRedirects, KeyError):
 		print('Error: connecting not successful(CoinGecko)')
 		return 0
 
 #priceFromCoinGecko()
+
+def txCRV():
+	tx = crv.functions.setPriceInUSD(1).build_transaction({
+		'nonce': w3.eth.get_transaction_count(address),
+		'chainId': 4,  # rinkeby
+		'gasPrice': price,
+		'gas': 3000000,  
+		})
+	signed_tx = w3.eth.account.sign_transaction(tx, private_key=SECRET)
+	res = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+	print(w3.toHex(res))
+#txCRV()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+''' Function for call smart contracts '''
+'''def callSmartContracts():
+	crv = w3.eth.contract(address=address_crv, abi=abi_crv)
+	weth = w3.eth.contract(address=address_weth, abi=abi_weth)
+	farm = w3.eth.contract(address=address_farm, abi=abi_farm)
+
+	addr_crv = crv.functions.addrCRV().call()
+	addr_weth = weth.functions.addrWETH().call()
+	addr_farm = farm.functions.addrFARM().call()
+	
+	print(addr_crv)
+	print(addr_weth)
+	print(addr_farm)
+
+#callSmartContracts()'''
 
 ''' Other network '''
 
