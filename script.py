c1f1b7dc75e8afcfffb8dfc88a48bc09a39936f1
CTAPCKPIM <novickdanya@gmail.com>
2022-09-24 16:04:56 +0300

    Sending prices on smart contracts (ETH network)

--- script.py
+++ script.py
@@ -17,8 +17,12 @@ from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 'load_dotenv()'' - load the configuration from a .env;
 'headers' - requesting JSON from the site;
 'API' - loading the variable;
-'web3' - connecting Web3;
+'INFURA' - your address of the provider; 
+'SECRET' - your private key;
+'w3' - connecting Web3;
 'eth_decimals' - decimals of USDC in ETH network;
+'eth_price_cc' - the array with price in USD(Cryptocompare);
+'eth_price_cg' - the array with price in USD(CoinGecko);
 """
 load_dotenv()
 
@@ -35,28 +39,29 @@ ccobj = cc.cryptocompare._set_api_key_parameter(API)
 cg = CoinGeckoAPI()
 w3 = Web3(Web3.HTTPProvider(INFURA))
 w3.eth.set_gas_price_strategy(rpc_gas_price_strategy)
-address = '0xf39D262c556aBefFC619847eE0355af42c4d0e91'
 
+# Arrays and variables for decimals 
 eth_decimals = 6
+eth_price_cc = []
+eth_price_cg = []
+#limit = 0
 
+address = '0xf39D262c556aBefFC619847eE0355af42c4d0e91'
 etherscan = 'https://api.etherscan.com/api?module=gastracker&action=gasoracle&apikey=YourApiKeyToken'
 
-''' ABIs and address of smart contracts(Ethereum network) '''
+""" ABIs and address of smart contracts(Ethereum network) """
 abi_crv = json.loads('[{"inputs":[{"internalType":"address","name":"_addrCRV","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"addrCRV","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"priceUSD","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"returnName","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"returnSymbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_priceUSD","type":"uint256"}],"name":"setPriceInUSD","outputs":[],"stateMutability":"nonpayable","type":"function"}]')
 abi_weth = json.loads('[{"inputs":[{"internalType":"address","name":"_addrWETH","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"addrWETH","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"priceUSD","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"returnName","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"returnSymbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_priceUSD","type":"uint256"}],"name":"setPriceInUSD","outputs":[],"stateMutability":"nonpayable","type":"function"}]')
 abi_farm = json.loads('[{"inputs":[{"internalType":"address","name":"_addrFARM","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"addrFARM","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"priceUSD","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"returnName","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"returnSymbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_priceUSD","type":"uint256"}],"name":"setPriceInUSD","outputs":[],"stateMutability":"nonpayable","type":"function"}]')
-
+# Address`s
 address_crv = '0x6DE6013B8Ea9fA78DB10e7D7e4D9E66668FaFAae'
 address_weth = '0x71815DcE02aAb149048e68c1EC777331fC890462'
 address_farm = '0xA64e04F3309794A7353894465B3e11D0007115aC'
 
+""" Connecting to smart contracts (ETH) """
 crv = w3.eth.contract(address=address_crv, abi=abi_crv)
 weth = w3.eth.contract(address=address_weth, abi=abi_weth)
 farm = w3.eth.contract(address=address_farm, abi=abi_farm)
-
-''' Other ABIs and adress '''
-
-#limit = 0
 
 """ Function for a control price of the gas 'Fantom' network """
 def getGasLimit():
@@ -99,9 +104,6 @@ def getGasPrise():
 	except (ConnectionError, Timeout, TooManyRedirects, KeyError):
 		return 0
 
-#price = getGasPrise()
-#print('Price of gas: ',price)
-
 """ Function parsing price of the cryptocurrency in USD from 'Cryptocompare'
 
 'currency_eth' - array of crypto currencys(CC)
@@ -117,7 +119,8 @@ def priceFromCryptocompare():
 		for i in currency_eth:
 			response = session.get('https://min-api.cryptocompare.com/data/price?fsym='+ i +'&tsyms=USD')
 			data_eth = json.loads(response.text) 
-			price = int(data_eth['USD'] * pow(10, eth_decimals)) 
+			price = int(data_eth['USD'] * pow(10, eth_decimals))
+			eth_price_cc.append(price) 
 	except (ConnectionError, Timeout, TooManyRedirects, KeyError):
 		print('Error: connecting not successful(Cryptocompare)')
 		return 0
@@ -141,52 +144,66 @@ def priceFromCoinGecko():
 			response = session.get('https://api.coingecko.com/api/v3/coins/' + i)
 			data_eth = json.loads(response.text)
 			usd_ = data_eth['market_data']['current_price']['usd']
-			price = int(usd_ * pow(10, eth_decimals)) 
+			price = int(usd_ * pow(10, eth_decimals))
+			eth_price_cg.append(price) 
 	except (ConnectionError, Timeout, TooManyRedirects, KeyError):
 		print('Error: connecting not successful(CoinGecko)')
 		return 0
 
 #priceFromCoinGecko()
 
-def txCRV():
-	tx = crv.functions.setPriceInUSD(1).build_transaction({
-		'nonce': w3.eth.get_transaction_count(address),
+""" A function for creating transactions in the ETH network 
+
+'gasPrice' - calling 'getGasPrise()' a function and getting a price on gas;
+'nonce' - getting a nonce from the address;
+"""
+def txsETH():
+	priceFromCryptocompare()
+
+	gasPrice = getGasPrise()
+	nonce = w3.eth.get_transaction_count(address)
+	txCRV = crv.functions.setPriceInUSD(eth_price_cc[0]).build_transaction({
+		'nonce': nonce,
 		'chainId': 4,  # rinkeby
-		'gasPrice': price,
+		'gasPrice': gasPrice,
 		'gas': 3000000,  
-		})
-	signed_tx = w3.eth.account.sign_transaction(tx, private_key=SECRET)
-	res = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
-	print(w3.toHex(res))
-#txCRV()
+	})
+	nonce +=1
+	txWETH = weth.functions.setPriceInUSD(eth_price_cc[1]).build_transaction({
+		'nonce': nonce,
+		'chainId': 4,  # rinkeby
+		'gasPrice': gasPrice,
+		'gas': 3000000,
+	})
+	nonce +=1
+	txFARM = farm.functions.setPriceInUSD(eth_price_cc[2]).build_transaction({
+		'nonce': nonce,
+		'chainId': 4,  # rinkeby 
+		'gasPrice': gasPrice,
+		'gas': 3000000,
+	})
+	# signing transactions
+	signed_tx_crv = w3.eth.account.sign_transaction(txCRV, private_key=SECRET)
+	signed_tx_weth = w3.eth.account.sign_transaction(txWETH, private_key=SECRET)
+	signed_tx_farm = w3.eth.account.sign_transaction(txFARM, private_key=SECRET)
+	res_crv = w3.eth.send_raw_transaction(signed_tx_crv.rawTransaction)
+	res_weth = w3.eth.send_raw_transaction(signed_tx_weth.rawTransaction)
+	res_farm = w3.eth.send_raw_transaction(signed_tx_farm.rawTransaction)
+	print(w3.toHex(res_crv))
+	print(w3.toHex(res_weth)) 
+	print(w3.toHex(res_farm))  
+
+#txsETH()
 
 
 
+#print(eth_price_cc[0])
+#print(eth_price_cc[1])
+#print(eth_price_cc[2])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#print(eth_price_cg[0])
+#print(eth_price_cg[1])
+#print(eth_price_cg[2])
 
 ''' Function for call smart contracts '''
 '''def callSmartContracts():
