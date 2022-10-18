4d0425dd2b8c328129ba3613cfe71574719eeb6c
CTAPCKPIM <novickdanya@gmail.com>
2022-10-18 23:36:27 +0300

    Added other networks, and minor tweaks

--- README.md
+++ README.md
@@ -1,10 +1,30 @@
 # Python script - 09/11/2022
 ---
 **PyScript** - we are **parsing** the **price** of tokens, using _CoinGecko_, and _Cryptocompare_, and then with help **transactions**, **setting** prices in _smart contracts_.
+
 ---
 ___Network___(**testnets**):
 > + **Ethereum**;
+>> _CRV_
+>> _WETH_
+>> _FARM_
 > + **Fantom**;
+>> _SPIRIT_
+>> _WFTM_
+>> _YFI_
+> + **Polygon**;
+>> _QUICK_
+>> _ORBS_
+>> _WMATIC_
+> + **AVAX**;
+>> _WAVAX_
+>> _SPELL_
+>> _PTP_
+> + **BSC**;
+>> _WBNB_
+>> _CAKE_
+>> _STG_
+
 ---
-(___project not completed.___)
+(___Completed.___)
 
--- script.py
+++ script.py
@@ -13,8 +13,11 @@ from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 load_dotenv()
 
-with open('dataset3.json') as f:
+with open('dataset.json') as f:
     data = json.load(f)
+
+with open('APIs.json') as f_:
+    APIs = json.load(f_)
 
 # 'headers' - requesting JSON from the site;
 headers = { 
@@ -24,28 +27,23 @@ headers = {
 """ All variables
 
 Dotenv variables:
- 'API' - loading the variable;
- 'INFURA' - your address of the provider; 
+ 'API' - loading the variable; 
  'SECRET' - your private key;
 
 Other variables:
  'cc' - connecting Сryptocompare;
  'cg' - connecting CoinGecko;
- 'w3' - connecting Web3;
- 'eth_decimals' - decimals of USDC in ETH network;
+ 'usdc_decimals' - decimals of USDC in networks;
 """
 API = os.getenv('API_KEY')
-INFURA = os.getenv('INFURA_URL')
 SECRET = os.getenv('SECRET_KEY')
 
 # Connecting providers
 cc = cc.cryptocompare._set_api_key_parameter(API)
 cg = CoinGeckoAPI()
-w3 = Web3(Web3.HTTPProvider(INFURA))
-w3.eth.set_gas_price_strategy(rpc_gas_price_strategy)
 
 # Variables for decimals 
-eth_decimals = 6
+usdc_decimals = 6
 address = '0xf39D262c556aBefFC619847eE0355af42c4d0e91'
 
 """ Function parsing price of the gas
@@ -53,20 +51,47 @@ address = '0xf39D262c556aBefFC619847eE0355af42c4d0e91'
  'data' - JSON from scans; 
  'gas_price' - price of the gas;
 """
-def getGasPrice(network): 
+def getGasPrice(network, chainId): 
 	session = Session()
 	session.headers.update(headers)
 	try:
 		response = session.get(network)
 		data = json.loads(response.text)
-		gas_price = int(float(data['result']['FastGasPrice']) * 1e9)
-		if (data['status'] == '1' and gas_price < gas_price * 5):
-			print('Price: Normal')
-			return gas_price
-		elif(data['status'] == '1' and gas_price > gas_price * 5):
-			print('Price: Overpriced') 
-			return gas_price * 5
-		else: return 0
+		if(chainId == 5 and data['status'] == '1'):
+			gas_price = int(float(data['result']['FastGasPrice']) * 1e9)
+			if(gas_price < gas_price * 5):
+				print('Price(gas): Normal')
+				return gas_price
+			else:
+				print('Price(gas): Overpriced')  
+				return gas_price * 5
+		if(chainId == 4002 and data['status'] == '1'):
+			gas_price = int(float(data['result']['FastGasPrice']) * 1e9)
+			if(gas_price < gas_price * 5):
+				print('Price(gas): Normal')
+				return gas_price
+			else:
+				print('Price(gas): Overpriced')  
+				return gas_price * 5
+		if(chainId == 80001 and data['status'] == '1'):
+			gas_price = int(float(data['result']['FastGasPrice']) * 1e9)
+			if(gas_price < gas_price * 5):
+				print('Price(gas): Normal')
+				return gas_price
+			else: 
+				print('Price(gas): Overpriced') 
+				return gas_price * 5
+		if(chainId == 97 and data['status'] == '1'):
+			gas_price = int(float(data['result']['FastGasPrice']) * 1e9)
+			if(gas_price < gas_price * 5):
+				print('Price(gas): Normal')
+				return gas_price
+			else: 
+				print('Price(gas): Overpriced') 
+				return gas_price * 5
+		if(chainId == 43113): 
+			print('Price(gas): Normal')
+			return int(data['result'], 16)
 	except (ConnectionError, Timeout, TooManyRedirects, KeyError):
 		return 0
 
@@ -79,21 +104,9 @@ def priceFromCryptocompare(currency):
 	session = Session()
 	session.headers.update(headers)  
 	try:
-		''' Ethereum network (USDC 6 decimals)'''
-		if currency == 'CRV' or currency == 'crv':
-			response = session.get('https://min-api.cryptocompare.com/data/price?fsym=CRV&tsyms=USD')
-			data_eth = json.loads(response.text) 
-			return int(data_eth['USD'] * pow(10, eth_decimals))
-		if currency == 'WETH' or currency == 'weth':
-			response = session.get('https://min-api.cryptocompare.com/data/price?fsym=WETH&tsyms=USD')
-			data_eth = json.loads(response.text) 
-			return int(data_eth['USD'] * pow(10, eth_decimals))
-		if currency == 'FARM' or currency == 'farm':
-			response = session.get('https://min-api.cryptocompare.com/data/price?fsym=FARM&tsyms=USD')
-			data_eth = json.loads(response.text) 
-			return int(data_eth['USD'] * pow(10, eth_decimals))
-		else: 
-			return 0
+		response = session.get('https://min-api.cryptocompare.com/data/price?fsym='+ currency +'&tsyms=USD')
+		data_eth = json.loads(response.text) 
+		return int(data_eth['USD'] * pow(10, usdc_decimals))
 	except (ConnectionError, Timeout, TooManyRedirects, KeyError):
 		print('Error: connecting not successful(Cryptocompare)')
 		return 0
@@ -108,59 +121,123 @@ def priceFromCoinGecko(currency):
 	session = Session()
 	session.headers.update(headers) 
 	try:
-		''' Ethereum network (USDC 6 decimals)'''
-		if currency == 'CRV' or currency == 'crv':
-			response = session.get('https://api.coingecko.com/api/v3/coins/curve-dao-token')
-			data_eth = json.loads(response.text)
-			usd_ = data_eth['market_data']['current_price']['usd']
-			return int(usd_ * pow(10, eth_decimals))
-		if currency == 'WETH' or currency == 'weth':
-			response = session.get('https://api.coingecko.com/api/v3/coins/weth')
-			data_eth = json.loads(response.text)
-			usd_ = data_eth['market_data']['current_price']['usd']
-			return int(usd_ * pow(10, eth_decimals))
-		if currency == 'FARM' or currency == 'farm':
-			response = session.get('https://api.coingecko.com/api/v3/coins/harvest-finance')
-			data_eth = json.loads(response.text)
-			usd_ = data_eth['market_data']['current_price']['usd']
-			return int(usd_ * pow(10, eth_decimals))
-		else: 
-			return 0
+		response = session.get('https://api.coingecko.com/api/v3/coins/' + currency)
+		data_eth = json.loads(response.text)
+		usd_ = data_eth['market_data']['current_price']['usd']
+		return int(usd_ * pow(10, usdc_decimals))
 	except (ConnectionError, Timeout, TooManyRedirects, KeyError):
 		print('Error: connecting not successful(CoinGecko)')
 		return 0
 
-""" A function for creating transactions 
+""" A function for creating transactions(Cryptocompare)
 
+'count' - number of the smart contract;
+'tokens' - array with tokens information;
+'api' - APIs of providers;
+'provider' - addresses of providers;
+'full' - full address after concatenation 'provider' + 'api';
+'w3' - connecting Web3;
+'nonce' - getting a nonce from the address;
+'abi_' - ABI of contracts;
+'address_' - address of contracts;
 'contract' - initializing an object 'contract';
 'price' - returning a price;
 'chainId' - returning an id of chain;
+'rpc' - address of the gas price;
 'gasPrice' - returning a gas price;
-'nonce' - getting a nonce from the address;
 """
-def testNEW():
-	nonce = w3.eth.get_transaction_count(address)
+def cryptocompareTransactions():
+	print('Cryptocompare:')
+	count = 0
 	for key in data:
 		tokens = key.get('tokens')
+		api = APIs[str(key.get('id'))]
+		provider = key.get('provider')
+		full = provider + api
+		w3 = Web3(Web3.HTTPProvider(full))
+		nonce = w3.eth.get_transaction_count(address)
+		w3.eth.set_gas_price_strategy(rpc_gas_price_strategy)
 		for token in tokens:
-			#import pdb;pdb.set_trace()
 			abi_ = token.get('abi')
 			address_ = token.get('address')
 			contract = w3.eth.contract(address=address_, abi=abi_)
 			price = priceFromCryptocompare(token.get('name'))
-			chainId = key.get('id') 
-			gasPrice = getGasPrice(key.get('url'))
+			chainId = key.get('id')
+			rpc = key.get('url') 
+			gasPrice = getGasPrice(rpc, chainId)
 			tx = contract.functions.setPriceInUSD(price
 				).build_transaction(
 				{
 					'nonce':nonce,
 					'chainId':chainId,
 					'gasPrice':gasPrice,
-					'gas':1000000,
+					'gas':450000,
 				})
 			nonce +=1
+			count +=1
 			signed_tx = w3.eth.account.sign_transaction(tx, private_key=SECRET)
 			res = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
-			print(f"Token: {token.get('name')} => {w3.toHex(res)}")
+			print(f"Contract№: {count}\nToken: {token.get('name')} => {w3.toHex(res)}\nPrice(USDC): {price}\n===\n")
 
-testNEW()
+""" A function for creating transactions(CoinGecko)
+
+'count' - number of the smart contract;
+'tokens' - array with tokens information;
+'api' - APIs of providers;
+'provider' - addresses of providers;
+'full' - full address after concatenation 'provider' + 'api';
+'w3' - connecting Web3;
+'nonce' - getting a nonce from the address;
+'abi_' - ABI of contracts;
+'address_' - address of contracts;
+'contract' - initializing an object 'contract';
+'price' - returning a price;
+'chainId' - returning an id of chain;
+'rpc' - address of the gas price;
+'gasPrice' - returning a gas price;
+"""
+def coinGeckoTransactions():
+	print('CoinGecko:')
+	count = 0
+	for key in data:
+		tokens = key.get('tokens')
+		api = APIs[str(key.get('id'))]
+		provider = key.get('provider')
+		full = provider + api
+		w3 = Web3(Web3.HTTPProvider(full))
+		nonce = w3.eth.get_transaction_count(address)
+		w3.eth.set_gas_price_strategy(rpc_gas_price_strategy)
+		for token in tokens:
+			abi_ = token.get('abi')
+			address_ = token.get('address')
+			contract = w3.eth.contract(address=address_, abi=abi_)
+			price = priceFromCoinGecko(token.get('longName'))
+			chainId = key.get('id')
+			rpc = key.get('url') 
+			gasPrice = getGasPrice(rpc, chainId)
+			tx = contract.functions.setPriceInUSD(price
+				).build_transaction(
+				{
+					'nonce':nonce,
+					'chainId':chainId,
+					'gasPrice':gasPrice,
+					'gas':450000,
+				})
+			nonce +=1
+			count +=1
+			signed_tx = w3.eth.account.sign_transaction(tx, private_key=SECRET)
+			res = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+			print(f"Contract№: {count}\nToken: {token.get('name')} => {w3.toHex(res)}\nPrice(USDC): {price}\n===\n")
+
+""" The primary function of a choice provider of prices
+
+ Using 'Cryptocompare', and CoinGecko services.
+"""
+def main():
+	try:
+		# If errors do not exist used 'cryptocompareTransactions()'
+		cryptocompareTransactions()
+	except (ConnectionError, Timeout, TooManyRedirects, KeyError):
+		# else used 'coinGeckoTransactions()'
+		coinGeckoTransactions()
+main()
