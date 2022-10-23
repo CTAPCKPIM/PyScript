0de8b2630928a7c9daad4168e69dac8695577c0f
CTAPCKPIM <novickdanya@gmail.com>
2022-10-23 18:14:50 +0300

    Tweaks: README.md/script.py/dataset.json

--- script.py
+++ script.py
@@ -129,91 +129,48 @@ def priceFromCoinGecko(currency):
 		print('Error: connecting not successful(CoinGecko)')
 		return 0
 
-""" A function for creating transactions(Cryptocompare)
+""" A function for creating transactions
 
 'count' - number of the smart contract;
+'price' - returning a price;
 'tokens' - array with tokens information;
 'api' - APIs of providers;
 'provider' - addresses of providers;
 'full' - full address after concatenation 'provider' + 'api';
+'url' - link of the scan;
+'scan' - keys of scans;
+'rpc' - full address of the gas price;
 'w3' - connecting Web3;
 'nonce' - getting a nonce from the address;
 'abi_' - ABI of contracts;
 'address_' - address of contracts;
 'contract' - initializing an object 'contract';
-'price' - returning a price;
 'chainId' - returning an id of chain;
-'rpc' - address of the gas price;
 'gasPrice' - returning a gas price;
 """
-def cryptocompareTransactions():
-	print('Cryptocompare:')
+def transactions(service):
 	count = 0
+	price = 0
 	for key in data:
 		tokens = key.get('tokens')
 		api = APIs[str(key.get('id'))]
 		provider = key.get('provider')
 		full = provider + api
+		url = key.get('url')
+		scan = APIs[api]
+		rpc = url + scan
 		w3 = Web3(Web3.HTTPProvider(full))
 		nonce = w3.eth.get_transaction_count(address)
 		w3.eth.set_gas_price_strategy(rpc_gas_price_strategy)
 		for token in tokens:
-			abi_ = token.get('abi')
+			abi_ = key.get('abi')
 			address_ = token.get('address')
 			contract = w3.eth.contract(address=address_, abi=abi_)
-			price = priceFromCryptocompare(token.get('name'))
-			chainId = key.get('id')
-			rpc = key.get('url') 
-			gasPrice = getGasPrice(rpc, chainId)
-			tx = contract.functions.setPriceInUSD(price
-				).build_transaction(
-				{
-					'nonce':nonce,
-					'chainId':chainId,
-					'gasPrice':gasPrice,
-					'gas':450000,
-				})
-			nonce +=1
-			count +=1
-			signed_tx = w3.eth.account.sign_transaction(tx, private_key=SECRET)
-			res = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
-			print(f"Contract№: {count}\nToken: {token.get('name')} => {w3.toHex(res)}\nPrice(USDC): {price}\n===\n")
-
-""" A function for creating transactions(CoinGecko)
-
-'count' - number of the smart contract;
-'tokens' - array with tokens information;
-'api' - APIs of providers;
-'provider' - addresses of providers;
-'full' - full address after concatenation 'provider' + 'api';
-'w3' - connecting Web3;
-'nonce' - getting a nonce from the address;
-'abi_' - ABI of contracts;
-'address_' - address of contracts;
-'contract' - initializing an object 'contract';
-'price' - returning a price;
-'chainId' - returning an id of chain;
-'rpc' - address of the gas price;
-'gasPrice' - returning a gas price;
-"""
-def coinGeckoTransactions():
-	print('CoinGecko:')
-	count = 0
-	for key in data:
-		tokens = key.get('tokens')
-		api = APIs[str(key.get('id'))]
-		provider = key.get('provider')
-		full = provider + api
-		w3 = Web3(Web3.HTTPProvider(full))
-		nonce = w3.eth.get_transaction_count(address)
-		w3.eth.set_gas_price_strategy(rpc_gas_price_strategy)
-		for token in tokens:
-			abi_ = token.get('abi')
-			address_ = token.get('address')
-			contract = w3.eth.contract(address=address_, abi=abi_)
-			price = priceFromCoinGecko(token.get('longName'))
-			chainId = key.get('id')
-			rpc = key.get('url') 
+			if(service == 'cc'):
+				price = priceFromCryptocompare(token.get('name'))
+			if(service == 'cg'):
+				price = priceFromCoinGecko(token.get('longName'))
+			chainId = key.get('id') 
 			gasPrice = getGasPrice(rpc, chainId)
 			tx = contract.functions.setPriceInUSD(price
 				).build_transaction(
@@ -231,13 +188,13 @@ def coinGeckoTransactions():
 
 """ The primary function of a choice provider of prices
 
- Using 'Cryptocompare', and CoinGecko services.
+ Using 'Cryptocompare', and 'CoinGecko' services.
 """
 def main():
 	try:
-		# If errors do not exist used 'cryptocompareTransactions()'
-		cryptocompareTransactions()
+		# If errors do not exist used 'Cryptocompare'
+		transactions('cc')
 	except (ConnectionError, Timeout, TooManyRedirects, KeyError):
-		# else used 'coinGeckoTransactions()'
-		coinGeckoTransactions()
+		# else used 'CoinGecko'
+		transactions('cg')
 main()
